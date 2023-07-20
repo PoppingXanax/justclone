af4a06f7b8e48afb1097d1949ebab462fefd4f2e
xander <139709574+PoppingXanax@users.noreply.github.com>
2023-07-20 15:52:49 +0100

    Add files via upload
    Fixed major bugs with scraping numerous pages.

--- gosrc/main.go
+++ gosrc/main.go
@@ -12,20 +12,17 @@ import (
 	"strings"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/common-nighthawk/go-figure"
 	"github.com/fatih/color"
-	"github.com/PuerkitoBio/goquery"
 )
 
 const (
-	cssDir     = "css"
-	jsDir      = "js"
-	imgDir     = "imgs"
-	fontDir    = "fonts"
-	pagesDir   = "pages"
-	statsTitle = "\033[32mScraping Statistics:\033[0m"
+	cssDir  = "css"
+	jsDir   = "js"
+	imgDir  = "imgs"
+	fontDir = "fonts"
 )
-
 
 type Statistics struct {
 	CSSFiles int
@@ -50,10 +47,6 @@ func main() {
 		return
 	}
 
-	// Flush the output buffer to ensure the new line is displayed
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
-
 	urls, err := readURLsFromFile("urls.txt")
 	if err != nil {
 		handleError(fmt.Sprintf("\nError reading URLs from file: %s\n", err))
@@ -62,38 +55,24 @@ func main() {
 
 	startTime := time.Now()
 
-	// Variables to keep track of total counts
-	var totalCSSFiles int
-	var totalJSFiles int
-	var totalImages int
-	var totalFonts int
+	stats := Statistics{}
 
 	for _, url := range urls {
 		cssCount, jsCount, imgCount, fontCount := scrapeWebsite(url)
-		totalCSSFiles += cssCount
-		totalJSFiles += jsCount
-		totalImages += imgCount
-		totalFonts += fontCount
+		stats.CSSFiles += cssCount
+		stats.JSFiles += jsCount
+		stats.Images += imgCount
+		stats.Fonts += fontCount
 	}
 
 	endTime := time.Now()
 	elapsedTime := endTime.Sub(startTime)
 	fmt.Printf("\nTask completed in %s\n", color.New(color.FgCyan).Sprint(elapsedTime))
-	fmt.Println()
-
-	stats := Statistics{
-		CSSFiles: totalCSSFiles,
-		JSFiles:  totalJSFiles,
-		Images:   totalImages,
-		Fonts:    totalFonts,
-	}
 
 	printStatistics(stats)
 
 	showHTMLFormatterNote()
 }
-
-
 
 func intro() {
 	asciiArt := figure.NewFigure("justClone", "", true)
@@ -105,7 +84,7 @@ func intro() {
 
 	fmt.Printf("%s %s\n", red("Dev Alert: There is a slight bug with fonts, but the cloned site should look 98% identical"), red("\n"))
 	fmt.Printf("%s ", cyan("[Console] =>"))
-	fmt.Printf("%s %s %s\n", white("Version"), cyan("0.0.2"), "| 20th July, 2023")
+	fmt.Printf("%s %s %s\n", white("Version"), cyan("0.1.0"), "| 20th July, 2023")
 	fmt.Printf("%s ", cyan("[Console] =>"))
 	fmt.Println("Coded by " + cyan("PoppingXanax"))
 	fmt.Printf("%s ", red("[INFO] =>"))
@@ -145,7 +124,10 @@ func scrapeWebsite(urlStr string) (int, int, int, int) {
 		return 0, 0, 0, 0
 	}
 
-	fmt.Printf("%sScrape in progress for %s...\n", color.New(color.FgYellow).Sprint("[Console] => "), color.New(color.FgCyan).Sprint(urlStr))
+	parsedURL, _ := url.Parse(urlStr)
+	pageName := getPageName(parsedURL.Path)
+
+	fmt.Printf("%sScraping %s...\n", color.New(color.FgYellow).Sprint("[Console] => "), color.New(color.FgCyan).Sprint(pageName))
 
 	if !strings.HasPrefix(urlStr, "http://") && !strings.HasPrefix(urlStr, "https://") {
 		urlStr = "https://" + urlStr
@@ -175,7 +157,45 @@ func scrapeWebsite(urlStr string) (int, int, int, int) {
 		return 0, 0, 0, 0
 	}
 
-	// Rest of the code for scraping and downloading assets
+	cssFiles := doc.Find("link[rel='stylesheet']")
+	cssCount := cssFiles.Length()
+	cssFiles.Each(func(i int, s *goquery.Selection) {
+		cssURL, _ := s.Attr("href")
+		filePath, err := downloadFile(resolveURL(urlStr, cssURL), filepath.Join(hostDirectory, cssDir), "CSS", getFileExtension(cssURL))
+		if err == nil {
+			s.SetAttr("href", fmt.Sprintf("%s/%s", cssDir, filepath.Base(filePath)))
+		}
+	})
+
+	jsFiles := doc.Find("script[src]")
+	jsCount := jsFiles.Length()
+	jsFiles.Each(func(i int, s *goquery.Selection) {
+		jsURL, _ := s.Attr("src")
+		filePath, err := downloadFile(resolveURL(urlStr, jsURL), filepath.Join(hostDirectory, jsDir), "JS", getFileExtension(jsURL))
+		if err == nil {
+			s.SetAttr("src", fmt.Sprintf("%s/%s", jsDir, filepath.Base(filePath)))
+		}
+	})
+
+	imgTags := doc.Find("img[src]")
+	imgCount := imgTags.Length()
+	imgTags.Each(func(i int, s *goquery.Selection) {
+		imgURL, _ := s.Attr("src")
+		filePath, err := downloadFile(resolveURL(urlStr, imgURL), filepath.Join(hostDirectory, imgDir), "IMG", getFileExtension(imgURL))
+		if err == nil {
+			s.SetAttr("src", fmt.Sprintf("%s/%s", imgDir, filepath.Base(filePath)))
+		}
+	})
+
+	fontTags := doc.Find("link[rel='stylesheet'][href$='.woff'], link[rel='stylesheet'][href$='.woff2'], link[rel='stylesheet'][href$='.ttf'], link[rel='stylesheet'][href$='.otf']")
+	fontCount := fontTags.Length()
+	fontTags.Each(func(i int, s *goquery.Selection) {
+		fontURL, _ := s.Attr("href")
+		filePath, err := downloadFile(resolveURL(urlStr, fontURL), filepath.Join(hostDirectory, fontDir), "FONT", getFileExtension(fontURL))
+		if err == nil {
+			s.SetAttr("href", fmt.Sprintf("%s/%s", fontDir, filepath.Base(filePath)))
+		}
+	})
 
 	htmlContent, err := doc.Html()
 	if err != nil {
@@ -190,22 +210,11 @@ func scrapeWebsite(urlStr string) (int, int, int, int) {
 		return 0, 0, 0, 0
 	}
 
-	fmt.Printf("%sScrape completed\n", color.New(color.FgGreen).Sprint("[Console] => "))
-
-	cssFiles := doc.Find("link[rel='stylesheet']")
-	cssCount := cssFiles.Length()
-
-	jsFiles := doc.Find("script[src]")
-	jsCount := jsFiles.Length()
-
-	imgTags := doc.Find("img[src]")
-	imgCount := imgTags.Length()
-
-	fontTags := doc.Find("link[rel='stylesheet'][href$='.woff'], link[rel='stylesheet'][href$='.woff2'], link[rel='stylesheet'][href$='.ttf'], link[rel='stylesheet'][href$='.otf']")
-	fontCount := fontTags.Length()
+	fmt.Printf("%sScraping %s completed\n", color.New(color.FgGreen).Sprint("[Console] => "), color.New(color.FgCyan).Sprint(pageName))
 
 	return cssCount, jsCount, imgCount, fontCount
 }
+
 
 func createDirectory(urlStr string) (string, error) {
 	parsedURL, err := url.Parse(urlStr)
@@ -214,31 +223,33 @@ func createDirectory(urlStr string) (string, error) {
 	}
 
 	hostDirectory := parsedURL.Host
-	pageDirectory := pagesDir
-	urlPath := strings.TrimPrefix(parsedURL.Path, "/")
+	pageName := getPageName(parsedURL.Path)
+	directoryPath := filepath.Join(hostDirectory, "pages", pageName)
 
-	directoryName := filepath.Join(hostDirectory, pageDirectory, urlPath)
-
-	if _, err := os.Stat(directoryName); os.IsNotExist(err) {
-		err := os.MkdirAll(directoryName, 0755)
+	if _, err := os.Stat(directoryPath); os.IsNotExist(err) {
+		err := os.MkdirAll(directoryPath, 0755)
 		if err != nil {
 			return "", err
 		}
 	}
 
-	return directoryName, nil
+	return directoryPath, nil
 }
 
-func createSubdirectories(directoryName string, subdirectories ...string) error {
-	for _, subdirectory := range subdirectories {
-		dirPath := filepath.Join(directoryName, subdirectory)
-		err := os.MkdirAll(dirPath, 0755)
-		if err != nil {
-			return err
-		}
+func getPageName(urlPath string) string {
+	if urlPath == "" || urlPath == "/" {
+		return "index"
 	}
-	return nil
+
+	base := path.Base(urlPath)
+	pageName := strings.TrimSuffix(base, path.Ext(base))
+	if pageName == "" {
+		return "index"
+	}
+
+	return pageName
 }
+
 
 func downloadFile(urlStr, directory, fileType, fileExt string) (string, error) {
 	parsedURL, err := url.Parse(urlStr)
@@ -253,6 +264,12 @@ func downloadFile(urlStr, directory, fileType, fileExt string) (string, error) {
 
 	if _, err := os.Stat(filePath); !os.IsNotExist(err) {
 		return filePath, nil
+	}
+
+	// Create the nested directories if they don't exist
+	err = os.MkdirAll(directory, 0755)
+	if err != nil {
+		return "", err
 	}
 
 	response, err := http.Get(urlStr)
@@ -279,6 +296,36 @@ func downloadFile(urlStr, directory, fileType, fileExt string) (string, error) {
 	return filePath, nil
 }
 
+
+func resolveURL(baseURL, href string) string {
+	base, _ := url.Parse(baseURL)
+	relative, _ := url.Parse(href)
+	return base.ResolveReference(relative).String()
+}
+
+func getFileExtension(filename string) string {
+	ext := path.Ext(filename)
+	return strings.ToLower(strings.TrimPrefix(ext, "."))
+}
+
+func printStatistics(stats Statistics) {
+	green := color.New(color.FgGreen).SprintFunc()
+
+	fmt.Println()
+	fmt.Printf("%s\n", green("Scraping Statistics:"))
+	fmt.Printf("Total CSS files: %s\n", color.New(color.FgCyan).Sprint(stats.CSSFiles))
+	fmt.Printf("Total JS files: %s\n", color.New(color.FgCyan).Sprint(stats.JSFiles))
+	fmt.Printf("Total images: %s\n", color.New(color.FgCyan).Sprint(stats.Images))
+	fmt.Printf("Total font files: %s\n", color.New(color.FgRed).Sprint(stats.Fonts))
+	fmt.Println()
+}
+
+
+
+func handleError(message string) {
+	color.Red(message)
+}
+
 func followRedirects(urlStr string) (string, error) {
 	client := &http.Client{}
 	resp, err := client.Get(urlStr)
@@ -290,44 +337,10 @@ func followRedirects(urlStr string) (string, error) {
 	return resp.Request.URL.String(), nil
 }
 
-func resolveURL(baseURL, href string) string {
-	base, err := url.Parse(baseURL)
-	if err != nil {
-		return href
-	}
-	rel, err := url.Parse(href)
-	if err != nil {
-		return href
-	}
-	return base.ResolveReference(rel).String()
-}
-
-func getFileExtension(url string) string {
-	ext := filepath.Ext(url)
-	if len(ext) > 1 {
-		return strings.TrimPrefix(ext, ".")
-	}
-	return ""
-}
-
-func handleError(message string) {
-	color.Red(message)
-}
-
-func printStatistics(stats Statistics) {
-	fmt.Println(statsTitle)
-	fmt.Printf("Total CSS files: %s\n", color.New(color.FgCyan).Sprint(stats.CSSFiles))
-	fmt.Printf("Total JS files: %s\n", color.New(color.FgCyan).Sprint(stats.JSFiles))
-	fmt.Printf("Total images: %s\n", color.New(color.FgCyan).Sprint(stats.Images))
-	fmt.Printf("Total font files: %s\n", color.New(color.FgRed).Sprint(stats.Fonts))
-	fmt.Println()
-}
-
 func showHTMLFormatterNote() {
 	red := color.New(color.FgRed).SprintFunc()
 	cyan := color.New(color.FgCyan).SprintFunc()
-	yellow := color.New(color.FgYellow).SprintFunc()
-	fmt.Printf("%s %s\n\n", yellow("Fonts should appear as normal, despite the tool stating 0 were found."), "")
 	fmt.Printf("%s %s\n", red("Note: I'm working on adding an HTML formatter. For now,"), "")
-	fmt.Printf("%s %s\n\n", red("please use"), cyan("https://www.freeformatter.com/html-formatter.html"))
+	fmt.Println(red("Please use") + " " + cyan("https://www.freeformatter.com/html-formatter.html"))
+	fmt.Println()
 }
